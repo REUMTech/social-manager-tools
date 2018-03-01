7888900e4db629c5c505ef8b2c58484de2a8f681
Francesco Gaudenzi <fgaudo@gmail.com>
2018-03-01 02:17:42 +0100

    Added save to config

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -11,17 +11,17 @@
             <div>
                 <div class="section mdc-elevation--z8">
                     <h1 class="title mdc-typography--Headline">Instagram settings</h1>
-                    <div class="mdc-text-field mdc-text-field--upgraded">
-                        <input required type="text" class="mdc-text-field__input" id="my-text-field" aria-controls="my-text-field-helper-text">
-                        <label for="my-text-field" class="mdc-text-field__label">Username</label>
+                    <div class="mdc-text-field mdc-text-field--upgraded" id="username">
+                        <input @input="saveToConfig" type="text" class="mdc-text-field__input" v-model="username" aria-controls="my-text-field-helper-text">
+                        <label for="username" class="mdc-text-field__label">Username</label>
                         <div class="mdc-line-ripple" style="transform-origin: 96.5px center"></div>
                     </div>
-                    <div class="mdc-text-field mdc-text-field--upgraded">
-                        <input required type="password" class="mdc-text-field__input" id="pw" aria-controls="my-text-field-helper-text">
-                        <label for="pw" class="mdc-text-field__label">Password</label>
+                    <div class="mdc-text-field mdc-text-field--upgraded" id="password">
+                        <input @input="saveToConfig" type="password" class="mdc-text-field__input" v-model="password" aria-controls="my-text-field-helper-text">
+                        <label for="password" class="mdc-text-field__label">Password</label>
                         <div class="mdc-line-ripple" style="transform-origin: 96.5px center"></div>
                     </div>
-                    <div class="mdc-text-field mdc-text-field--upgraded">
+                    <div class="mdc-text-field mdc-text-field--upgraded" id="tagsList">
                         <input @keydown="verifyTag" type="text" class="mdc-text-field__input" id="tags" aria-controls="my-text-field-helper-text">
                         <label for="tags" class="mdc-text-field__label">Add tags</label>
                         <div class="mdc-line-ripple" style="transform-origin: 96.5px center"></div>
@@ -67,26 +67,42 @@
 <script>
 import {MDCSelect} from '@material/select'
 import {MDCTextField} from '@material/textfield'
-import {MDCChip, MDCChipSet} from '@material/chips'
+import fs from 'fs'
 
 export default {
   mounted () {
-    MDCChipSet.attachTo(this.$el.querySelector('.mdc-chip-set'))
-    this.$el.querySelectorAll('.mdc-chip').forEach(element => {
-      MDCChip.attachTo(element)
-    })
+    this.readConfig()
 
     MDCSelect.attachTo(this.$el.querySelector('.mdc-select'))
-    this.$el.querySelectorAll('.mdc-text-field').forEach(element => {
-      MDCTextField.attachTo(element)
-    })
+    MDCTextField.attachTo(this.$el.querySelector('#username')).value = this.username
+    MDCTextField.attachTo(this.$el.querySelector('#password')).value = this.password
+    MDCTextField.attachTo(this.$el.querySelector('#tagsList'))
   },
   data: function () {
     return {
+      username: '',
+      password: '',
       hashtags: []
     }
   },
   methods: {
+    readConfig: function () {
+      if (!fs.existsSync('config.json')) {
+        return
+      }
+      const data = JSON.parse(fs.readFileSync('config.json', 'utf-8'))
+      this.username = data.username
+      this.password = atob(data.password)
+      this.hashtags = data.hashtags
+    },
+    saveToConfig: function () {
+      const data = {
+        username: this.username,
+        password: btoa(this.password),
+        hashtags: this.hashtags
+      }
+      fs.writeFileSync('config.json', JSON.stringify(data, null, 2), 'utf-8')
+    },
     verifyTag: function (event) {
       event.target.value = event.target.value.trim()
       const isCommand = (event.code === 'Space' || event.code === 'Enter' || event.code === 'Comma')
@@ -100,12 +116,14 @@ export default {
       if (!this.$data.hashtags.includes(tag)) {
         this.$data.hashtags.push(tag)
       }
+      this.saveToConfig()
     },
     removeHashtag: function (tag) {
       const position = this.$data.hashtags.indexOf(tag)
       if (position > -1) {
         this.$data.hashtags.splice(position, 1)
       }
+      this.saveToConfig()
     }
   }
 }
@@ -123,14 +141,14 @@ export default {
     }
 
     .list-item {
-    transition: all 1s;
+    transition: all .4s;
     display: inline-block;
     margin-right: 10px;
     }
     .list-enter, .list-leave-to
     /* .list-complete-leave-active below version 2.1.8 */ {
     opacity: 0;
-    transform: translateY(30px);
+    transform: translateY(60px);
     }
     .list-leave-active {
     position: absolute;
